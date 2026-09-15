5077baaa245f70d679b13bac8a9c0ea2c6edcd2e
mohammedasim7020 <asimferoz555@gmail.com>
2026-09-15 20:17:05 +0530

    learning backend

--- Day1/day1.js
+++ Day1/day1.js
@@ -7,12 +7,12 @@ console.log(greeting);
 const name = "Asim";
 console.log(`Hello, ${name}! Let's get started with some coding exercises.`);
 
-console.log(process.version);
+console.log(process.version); // for getting the version of Node.js
 
-console.log(process.platform);
+console.log(process.platform); // for getting the platform of the operating system
 
-console.log(__dirname);
+console.log(__dirname); // for getting the current directory path
 
-console.log(__filename);
+console.log(__filename); // for getting the current file path
 
-console.log(globalThis === global); 
+console.log(globalThis === global); // for checking if globalThis is the same as global
